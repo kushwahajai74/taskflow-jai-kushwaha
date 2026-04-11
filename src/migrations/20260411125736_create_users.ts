f51e6b3aa97ed15d98dd802e605738254a237261
jai-touchkin <jai.kushwaha@touchkin.com>
2026-04-11 19:02:57 +0530

    Add migration to create users table with necessary fields

---
+++ src/migrations/20260411125736_create_users.ts
@@ -0,0 +1,15 @@
+import type { Knex } from "knex";
+
+export async function up(knex: Knex): Promise<void> {
+  await knex.schema.createTable("users", (table) => {
+    table.uuid("id").primary().defaultTo(knex.fn.uuid());
+    table.string("name").notNullable();
+    table.string("email").unique().notNullable();
+    table.string("password").notNullable();
+    table.timestamp("created_at").defaultTo(knex.fn.now());
+  });
+}
+
+export async function down(knex: Knex): Promise<void> {
+  await knex.schema.dropTable("users");
+}
